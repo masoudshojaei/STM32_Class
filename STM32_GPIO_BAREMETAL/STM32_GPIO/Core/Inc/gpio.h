2307f9fa6fdd5d7ad1e9276d56037896e816c4ac
Masoud Shojaei <masoud@stemsrl.it>
2026-06-24 11:10:26 +0200

    Session 4

---
+++ STM32_GPIO_BAREMETAL/STM32_GPIO/Core/Inc/gpio.h
@@ -0,0 +1,18 @@
+/*
+ * gpio.h
+ *
+ *  Created on: Jun 11, 2026
+ *      Author: shoja
+ */
+
+#ifndef INC_GPIO_H_
+#define INC_GPIO_H_
+
+void LED_init (void);
+void LED_Blink_Fast(void);
+void OUTPUT_init_PB8 (void);
+uint8_t OUTPUT_PB8_Blink_Fast(void);
+void OUTPUT_init_PC7 (void);
+uint8_t OUTPUT_PC7_Blink_Fast(void);
+void Police_Blink_Fast(void);
+#endif /* INC_GPIO_H_ */
